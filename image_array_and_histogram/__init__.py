34c52369266d88ffb9408bf995a2f23932c8c967
rishi-chauhan <rishi-chauhan@users.noreply.github.com>
2021-02-04 23:47:35 +0530

    Fixed setup file and removed -rrsc from package name

--- image_array_and_histogram/__init__.py
+++ image_array_and_histogram/__init__.py
@@ -1,4 +1,4 @@
-import main
+from image_array_and_histogram.main import getHist, getImageArray, getImageFromArray
 
 __name__ = 'Image array and histogram'
 __author__ = 'Rishi Raj Singh Chauhan <rishirschauhan@gmail.com>'
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='image_array_and_histogram',
-    version='0.0.3',
+    version='1.0.0',
     author='Rishi Raj Singh Chauhan',
     author_email='rishirschauhan@gmail.com',
     description='Helps in getting array and histogram of a greyscale image. Also, image from the array.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/rishi-chauhan/my-packages.git',
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
